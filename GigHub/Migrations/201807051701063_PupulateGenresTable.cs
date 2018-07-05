42838dfa8673e72c4d1c3026985317cb266ec6b2
Flavio Campelo <flaviocampelo@hotmail.com>
2018-07-05 14:03:54 -0300

    -Populating the database

---
+++ GigHub/Migrations/201807051701063_PupulateGenresTable.cs
@@ -0,0 +1,20 @@
+namespace GigHub.Migrations
+{
+  using System.Data.Entity.Migrations;
+
+  public partial class PupulateGenresTable : DbMigration
+  {
+    public override void Up()
+    {
+      Sql("INSERT INTO GENRES (ID, NAME) VALUES (1,'Jazz')");
+      Sql("INSERT INTO GENRES (ID, NAME) VALUES (2,'Blues')");
+      Sql("INSERT INTO GENRES (ID, NAME) VALUES (3,'Rock')");
+      Sql("INSERT INTO GENRES (ID, NAME) VALUES (4,'Country')");
+    }
+
+    public override void Down()
+    {
+      Sql("DELETE FROM GENRES WHERE ID IN (1, 2, 3, 4)");
+    }
+  }
+}
