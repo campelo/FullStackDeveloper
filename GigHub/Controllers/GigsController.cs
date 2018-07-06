b9fde2d7e565e748784904fba2d32c42c4034338
Flavio Campelo <flaviocampelo@hotmail.com>
2018-07-06 07:52:08 -0300

    -Adding a drop-down list

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GigHub.Models;
+using GigHub.ViewModels;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
-    public class GigsController : Controller
+  public class GigsController : Controller
+  {
+    private ApplicationDbContext _context;
+
+    public GigsController()
     {
-        // GET: Gigs
-        public ActionResult Create()
-        {
-            return View();
-        }
+      _context = new ApplicationDbContext();
     }
+
+    // GET: Gigs
+    public ActionResult Create()
+    {
+      var viewModel = new GigFormViewModel
+      {
+        Genres = _context.Genres.ToList()
+      };
+      return View(viewModel);
+    }
+  }
 }
