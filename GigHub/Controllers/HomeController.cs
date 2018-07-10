892559a4209ca0e9b9467152c0e6a970903e1651
Flavio Campelo <flaviocampelo@hotmail.com>
2018-07-10 07:52:00 -0300

    -Building a basic view

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -1,30 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GigHub.Models;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private ApplicationDbContext _context;
+
+    public HomeController()
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+      _context = new ApplicationDbContext();
     }
+
+    public ActionResult Index()
+    {
+      var upcomingGigs = _context.Gigs
+        .Include(g => g.Artist)
+        .Where(g => g.DateTime > DateTime.Now);
+
+      return View(upcomingGigs);
+    }
+
+    public ActionResult About()
+    {
+      ViewBag.Message = "Your application description page.";
+
+      return View();
+    }
+
+    public ActionResult Contact()
+    {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+  }
 }
