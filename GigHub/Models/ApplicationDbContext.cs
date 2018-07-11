ac67639852ad305140581fe32dda848db0cc92fc
Flavio <flaviocampelo@hotmail.com>
2018-07-10 21:47:33 -0300

    -Extending the domain model

--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -4,18 +4,19 @@ using Microsoft.AspNet.Identity.EntityFramework;
 namespace GigHub.Models
 {
   public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-      public DbSet<Gig> Gigs { get; set; }
+  {
+    public DbSet<Gig> Gigs { get; set; }
     public DbSet<Genre> Genres { get; set; }
+    public DbSet<Attendance> Attendances { get; set; }
 
     public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
+    {
     }
+
+    public static ApplicationDbContext Create()
+    {
+      return new ApplicationDbContext();
+    }
+  }
 }
